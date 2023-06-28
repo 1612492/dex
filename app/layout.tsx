cc40ec5a9a20ac629af277b015b6d6b33a3e0a66
1612492 <39008689+1612492@users.noreply.github.com>
2023-06-28 11:28:13 +0700

    feat: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,27 @@ const poppins = Poppins({ weight: ['400', '500', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'DEX',
+  description: 'DEX boilerplate',
+  themeColor: 'fff',
+  manifest: '/manifest.json',
+  metadataBase: new URL('https://dex-boilerplate.vercel.app'),
+  icons: [
+    { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/icons/16.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/icons/32.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', url: '/icons/180.png' },
+  ],
+  openGraph: {
+    title: 'DEX',
+    description: 'DEX boilerplate',
+    url: 'https://dex-boilerplate.vercel.app',
+    images: '/og.png',
+  },
+  twitter: {
+    title: 'DEX',
+    description: 'DEX boilerplate',
+    card: 'summary_large_image',
+    images: '/twitter-og.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
